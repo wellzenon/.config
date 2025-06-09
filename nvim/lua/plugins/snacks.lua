a1969d085a9cb231ed079a0c425db717ce395f62
Wellington Zenon <wellington.zenon@gmail.com>
2025-06-09 18:31:31 -0300

    feat: Adicionar seletor Treesitter

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -101,6 +101,7 @@ return {
     -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Document Symbols" },
     { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    { "<leader>st", function() Snacks.picker.treesitter() end, desc = "Treesitter" },
     -- Other
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
